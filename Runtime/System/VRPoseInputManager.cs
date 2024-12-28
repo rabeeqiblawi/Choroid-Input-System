57aa39be08edd3ee388d09425ca19d0b2dc7febc
Rabee Qiblawi <rabeekiblawi@gmail.com>
2024-12-28 19:28:13 +0200

    added debug ui

--- Runtime/System/VRPoseInputManager.cs
+++ Runtime/System/VRPoseInputManager.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using TMPro;
 using UnityEngine;
 
 namespace ChoroidXR.VRposeInputMapper
@@ -9,9 +11,43 @@ namespace ChoroidXR.VRposeInputMapper
         public Transform hmd;
 
         public static VRPoseInputManager Instance;
-        private void Awake()
+        public TMP_Text debugText;
+        private StringBuilder logBuilder = new StringBuilder();
+
+        protected void Awake()
         {
-            Instance = this;
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        public void XRLog(string message)
+        {
+            logBuilder.AppendLine(message);
+            debugText.text = logBuilder.ToString();
+            Debug.Log(message);
+        }
+
+        protected void LateUpdate()
+        {
+            logBuilder.Clear();
+        }
+    }
+    public static class XRUtils
+    {
+        public static void Log(string message)
+        {
+            VRPoseInputManager.Instance.XRLog(message);
+            Debug.Log(message);
         }
     }
 }
+
+
+
